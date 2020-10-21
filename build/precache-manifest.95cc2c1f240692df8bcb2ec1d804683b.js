31fbeb8b4dc711e97830a7db17b878b11128e474
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-10-21 16:04:24 -0700

    changed title and changed favicon

---
+++ build/precache-manifest.95cc2c1f240692df8bcb2ec1d804683b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f493f8d0bc760799438d6de438d83e43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d045e525e39dd07ae8f3",
+    "url": "/static/css/main.7e00e6bc.chunk.css"
+  },
+  {
+    "revision": "7ddd6f2987ec683b28b7",
+    "url": "/static/js/2.05ed3d02.chunk.js"
+  },
+  {
+    "revision": "40ff5562bfbe0dc926d145d5290e83c2",
+    "url": "/static/js/2.05ed3d02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d045e525e39dd07ae8f3",
+    "url": "/static/js/main.426f2248.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
